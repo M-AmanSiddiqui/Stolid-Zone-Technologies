3afa7be25ae04aa229380146dd93eb7905710375
Muhammad Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-01-15 22:55:53 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Ensure this path is correct
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure this includes all files where you use Tailwind classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

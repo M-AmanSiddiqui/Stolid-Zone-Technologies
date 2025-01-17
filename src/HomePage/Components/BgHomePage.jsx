5b194f5e098870698eb97ec0250d6ba6645b870e
Muhammad Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-01-17 04:47:36 -0800

    add heading

--- src/HomePage/Components/BgHomePage.jsx
+++ src/HomePage/Components/BgHomePage.jsx
@@ -2,7 +2,7 @@ import BgHero from "../../assets/Images/BgHomePage.mp4";
 
 function BgHomePage() {
   return (
-    <div className="relative w-full h-screen overflow-hidden z-0">
+    <div className="relative w-full h-screen overflow-hidden">
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
         autoPlay
@@ -11,9 +11,17 @@ function BgHomePage() {
         playsInline
       >
         <source src={BgHero} type="video/mp4" />
-        
       </video>
-      <h1 className="bg-black absolute">abb</h1>
+      <div className="relative z-10 flex  items-center w-full h-full ml-80 -mt-24 bg-black bg-opacity-0">
+        <div>
+      <h1 className="text-black text-7xl font-inter font-bold whitespace-normal ">
+  The Brand 
+</h1>
+<h1 className="text-black text-7xl font-inter font-bold whitespace-normal">
+   You Want Here!
+</h1>
+</div>
+      </div>
     </div>
   );
 }

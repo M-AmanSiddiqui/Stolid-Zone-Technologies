f4b4285c736368c659d1b3d190983e86c1d82b3f
Muhammad Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-01-23 04:23:05 -0800

    half work

--- src/HomePage/Components/Agency.jsx
+++ src/HomePage/Components/Agency.jsx
@@ -2,8 +2,10 @@
 
 export default function Agency() {
   return (
-    <div>
-      <h1 className="bg-white"> sassss</h1>
+    <div className="bg-white px-72">
+      <h4 className="text-blue-800 bg-gray-50 ">About Us </h4>
+      <h1 className="text-6xl font-semibold">Meet our expert dynamic <br /> digital agency team</h1>
+      <p className="mt-12 text-gray-500">Operational efficiency is another critical area where we provide substantial value. By <br /> analyzing existing processes and workflows, we identify inefficiencies and bottlenecks that <br /> can be eliminated or improved.</p>
     </div>
   )
 }

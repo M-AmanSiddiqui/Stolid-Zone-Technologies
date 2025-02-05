8c46d3dddab02e35f9220dd5d9d94ff47c856d43
Muhammad Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-02-05 02:32:41 -0800

    workin on boost sale page

--- src/Services/BOOST YOUR SALES/BoostSales.jsx
+++ src/Services/BOOST YOUR SALES/BoostSales.jsx
@@ -1,5 +1,10 @@
 import Header from "../../Component/Header"
-import salegraph from "../../assets/Images/boost Sale Graph.png"
+import salegraph from "../../assets/Images/growth.png"
+import listingimage from "../../assets/Images/listing image.jpg"
+import growing from "../../assets/Images/growing.jpg"
+import ppcDashboard from "../../assets/Images/ppc dashboard.jpg"
+import SatisfyTeam from "../../assets/Images/satisfy  team.jpg"
+import BoostSaleGraph from "../../assets/Images/sucess.webp"
 // import Footer from "../../Component/Footer"
  function BoostSales() {
   return (
@@ -9,12 +14,67 @@ import salegraph from "../../assets/Images/boost Sale Graph.png"
 
       <h1 className="mt-72 font-extrabold text-5xl text-center">BOOST YOUR SALES</h1>
       <div className="flex flex-col justify-center items-center">
-  <img src={salegraph} alt="" className="bg-black" />
+  <img src={salegraph} alt="" className="bg-black h-96" />
   <h2 className="mt-4 font-bold text-center">Maximize Your Sales with Targeted Strategies</h2>
   <p className="mt-1 font- text-center max-w-xl">
     Increasing your sales on Amazon requires more than just having a good product. It’s about ensuring your listings are optimized, your products are discoverable, and your ads are targeted to the right audience. Our Boost Your Sales service focuses on driving high-quality traffic to your listings while ensuring you’re making the most of every opportunity Amazon offers.
   </p>
 </div>
+
+
+<div className="mt-20 ml-10 flex justify-between">
+ 
+  <div className="flex flex-col items-center">
+    <img src={listingimage} alt="" className="bg-black h-96" />
+    <h2 className="mt-4 font-bold text-center">Optimizing Listings and Product Visibility</h2>
+    <p className="mt-1 text-center max-w-xl">
+    We start by enhancing your product listings—optimizing titles, descriptions, and keywords to rank higher in Amazon search results. By using targeted, data-driven strategies, we increase visibility, making your products stand out to potential buyers. Higher visibility means more clicks, and more clicks lead to increased conversions.   </p>
+  </div>
+
+ 
+  <div className="flex mr-10 flex-col items-center">
+    <img src={ppcDashboard} alt="" className="bg-black h-96" />
+    <h2 className="mt-4 font-bold text-center">Driving Traffic with Amazon Ads</h2>
+    <p className="mt-1 text-center max-w-xl">
+    Running effective ads on Amazon is key to boosting sales. We specialize in Amazon PPC (Pay-Per-Click) campaigns that target the right customers based on their browsing habits. Our team ensures that your budget is spent wisely, focusing on ads that give you the best return on investment. Through continuous optimization, we ensure that your ads generate quality traffic, turning visitors into buyers.  </p>
+  </div>
+</div>
+
+
+<div className="flex mt-12 flex-col items-center">
+    <img src={growing} alt="" className="bg-black h-96" />
+    <h2 className="mt-4 font-bold text-center">Continuous Optimization and Scaling</h2>
+    <p className="mt-1 text-center max-w-xl">
+    Sales success doesn’t end once we’ve optimized your listings and run ads. Our service includes ongoing monitoring and fine-tuning to ensure your sales grow over time. We constantly analyze the performance of your listings and ads, making adjustments to maintain and increase your sales in the long run. From seasonal promotions to special offers, we continuously find new ways to scale your sales.
+    </p>
+  </div>
+
+
+
+
+
+
+  <div className="mt-20 ml-10 flex justify-between">
+ 
+ <div className="flex flex-col items-center">
+   <img src={SatisfyTeam} alt="" className="bg-black h-96" />
+   <h2 className="mt-4 font-bold text-center">Why Choose Our Boost Your Sales Service?</h2>
+   <p className="mt-1 text-center max-w-xl">
+   We don’t just focus on short-term gains; our strategies are designed to build a strong foundation for long-term growth. With our expert team’s deep understanding of Amazon’s ecosystem, we know what works—and what doesn’t. By partnering with us, you’ll see measurable improvements in your sales and brand visibility on Amazon.   </p>
+ </div>
+
+
+ <div className="flex mr-10 flex-col items-center">
+   <img src={BoostSaleGraph} alt="" className="bg-black h-96" />
+   <h2 className="mt-4 font-bold text-center">Driving Traffic with Amazon Ads</h2>
+   <p className="mt-1 text-center max-w-xl">
+   Running effective ads on Amazon is key to boosting sales. We specialize in Amazon PPC (Pay-Per-Click) campaigns that target the right customers based on their browsing habits. Our team ensures that your budget is spent wisely, focusing on ads that give you the best return on investment. Through continuous optimization, we ensure that your ads generate quality traffic, turning visitors into buyers.  </p>
+ </div>
+</div>
+
+
+  
+
 
 
 
